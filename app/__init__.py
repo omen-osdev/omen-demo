c6278e414aa7241f7be266a6a80518f3e89a1e73
Yon <ejliud@gmail.com>
2024-09-13 21:58:43 +0300

    docs: Modify .gitignore to NOT upload any image files except original image & Reduce comments in __init__.py
    - Add 18*.img to .gitignore
    - Improve readability by reducing spaces

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,7 @@ from flask import Flask, render_template, session
 from flask_session import Session
 from cachelib.simple import SimpleCache
 
-
 INSTANCE_LIFETIME = 1 # Lifetime of each instance, in minutes
-
-
 
 def create_app(test_config=None):
     """
@@ -25,7 +22,6 @@ def create_app(test_config=None):
     
     """
 
-
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
@@ -35,13 +31,12 @@ def create_app(test_config=None):
         SESSION_CACHELIB = SimpleCache(threshold=500, default_timeout=300),
         SESSION_PERMANENT = False,
         PERMANENT_SESSION_LIFETIME = timedelta(minutes=INSTANCE_LIFETIME),
-
-
     )
 
-    # Start server-side sessions(TODO: Change to Redis)
     # TODO: Follow before deploying to prod: https://flask-session.readthedocs.io/en/latest/security.html
     # TODO: Consider using flask-talisman: https://github.com/wntrblm/flask-talisman
+    # Start server-side sessions
+    #TODO: Change to Redis
     Session(app)
 
     if test_config is None:
@@ -57,26 +52,21 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # use test_config here
+    #TODO: Use test_config to set up the app for testing here
 
-    
     from .instance import init_launcher
-    init_launcher()    # Init the instance launcher
+    init_launcher()    # Init the instance launcher #? Should this be done here? Maybe an HTTP Method should be used to start the instance
     # TODO: call cleanup_launcher in some sort of a cleanup function
-
 
     @app.route('/')
     def home():
         return render_template("index.html")
 
-
     from . import instance_page
     app.register_blueprint(instance_page.bp)
     
-
     return app
-    
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
