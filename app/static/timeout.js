a41e6c761110fb0682ac3518b9470f80537b5f9f
Yon <ejliud@gmail.com>
2024-09-15 21:00:55 +0300

    fix: Countdown now works
    - Linked timeout.js to instance.html
    - Got countdown to work

--- app/static/timeout.js
+++ app/static/timeout.js
@@ -1,13 +1,13 @@
-let timeout = timeout / 1000; // convert to seconds
+let new_timeout = timeout / 1000; // convert to seconds
 
 function startCountdown() {
   const timerElement = document.getElementById("countdown");
 
   const interval = setInterval(() => {
-    timerElement.textContent = `Redirecting in ${timeout} seconds...`;
-    timeout--;
+    timerElement.textContent = `Redirecting in ${new_timeout} seconds...`;
+    new_timeout--;
 
-    if (timeout === 0) {
+    if (new_timeout === 0) {
       clearInterval(interval);
       timerElement.textContent = "Redirecting...";
       window.location.href = "/"; 
