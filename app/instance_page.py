ca2a6761236397e18866012824ac4d8c9bf200d2
Yon <ejliud@gmail.com>
2024-09-19 17:58:11 +0300

    chore: "beautify" `instance_page.py`
    - Make it more readable

--- app/instance_page.py
+++ app/instance_page.py
@@ -13,21 +13,24 @@ from app import INSTANCE_LIFETIME
 
 bp = Blueprint("instance_page", __name__, url_prefix="/instance")
 
-# Note: Please docker kill <id> manually for now :)
+
 @bp.route("/", methods=["GET"])
 def launch_instance():
-    # Make sure that we can reach here only after passing the captcha
     if session.get("instance_key", None) is None:
-        current_app.session_interface.regenerate(session)   # Protection against session fixation attacks
+        current_app.session_interface.regenerate(session)
         port = create_instance()
+
         if port == -1:
             return "Could not create an instance!"
         instance = InstanceModel(port, int(time.time()))
         session["instance_key"] = instance
-        
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         loop = asyncio.get_event_loop()
-        loop.run_in_executor(None, loop.run_until_complete, instance.check_and_shutdown(INSTANCE_LIFETIME * 60))
+        loop.run_in_executor(
+                None,
+                loop.run_until_complete,
+                instance.check_and_shutdown(INSTANCE_LIFETIME * 60)
+        )
 
     return render_template("instance.html")
