2753f48b074e91732909db25f32e3ced8e7dcf48
Yon <ejliud@gmail.com>
2024-09-19 22:16:52 +0300

    chore: Add more error handling
    - Add `try` statement to handle any unexpected crashes within instance
    - Add handling incase an `instance_key` already exists in the current
    session

--- app/instance_page.py
+++ app/instance_page.py
@@ -16,21 +16,30 @@ bp = Blueprint("instance_page", __name__, url_prefix="/instance")
 
 @bp.route("/", methods=["GET"])
 def launch_instance():
-    if session.get("instance_key", None) is None:
-        current_app.session_interface.regenerate(session)
-        port = create_instance()
+    try:
+        if session.get("instance_key", None) is None:
+            current_app.session_interface.regenerate(session)
+            port = create_instance()
+    
+            if port == -1:
+                return render_template("error.html", error="Unable to create a new instance")
+            instance = InstanceModel(port, int(time.time()))
+            session["instance_key"] = instance
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            loop = asyncio.get_event_loop()
+            loop.run_in_executor(
+                    None,
+                    loop.run_until_complete,
+                    instance.check_and_shutdown(INSTANCE_LIFETIME * 60)
+            )
 
-        if port == -1:
-            return render_template("error.html", error="Unable to create a new instance")
-        instance = InstanceModel(port, int(time.time()))
-        session["instance_key"] = instance
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop = asyncio.get_event_loop()
-        loop.run_in_executor(
-                None,
-                loop.run_until_complete,
-                instance.check_and_shutdown(INSTANCE_LIFETIME * 60)
-        )
-
-    return render_template("instance.html")
+        return render_template("instance.html")
+        else:
+            return render_template("error.html", error="You already have an instance running, please close it before launching a new one.")
+            # TODO:
+            # Add a button to close the instance
+            # *OR*
+            # just redirect to the original instance
+        except Exception as e:
+            return render_template("error.html", error=str(e))
